ee131432293b23b7be14a805a5db36317e4e0931
Victor <72082776+kotop21@users.noreply.github.com>
2026-03-21 17:37:14 +0200

    Fix: absolute dir

--- src/config.py
+++ src/config.py
@@ -1,10 +1,17 @@
 import os
+import sys
 from utils.user_config import ConfigManager
 
 zerotier_id = "ebe7fbd445156789"
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-FONT_PATH = os.path.join(BASE_DIR, "assets", "Roboto-Regular.ttf")
+
+def resource_path(relative_path):
+    base_path = getattr(sys, "_MEIPASS", os.path.abspath("."))
+
+    return os.path.join(base_path, relative_path)
+
+
+FONT_PATH = resource_path(os.path.join("assets", "Roboto-Regular.ttf"))
 
 PORT = 9911
 
